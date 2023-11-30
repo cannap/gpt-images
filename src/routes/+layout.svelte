6b5c96c9328932b77a00960a20fa942d7c293f1f
Marko Bolliger <sh4fty@gmail.com>
2023-11-30 22:13:21 +0100

    more tests

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
-<script>
+<script lang="ts">
 	import '../app.scss';
+	import { page } from '$app/stores';
+
 	import { base } from '$app/paths';
+	import { text } from '@sveltejs/kit';
+
+	$: isActive = (route: string) => {
+		return $page.route.id === route;
+	};
 </script>
 
-<nav>
-	<a href="{base}/">Home</a>
-	<a href="{base}/test">Test</a>
-</nav>
-<div class="container">
-	<slot />
+<a c class:text-red-600={isActive('/')} href="{base}/">Index</a>
+<a class:text-red-600={isActive('/test')} href="{base}/test">Test page</a>
+<div class="flex flex-col h-screen">
+	<header class="bg-red-500">header</header>
+	<main class="bg-green-500 flex-grow"><slot /></main>
+	<footer class="bg-blue-500">footer</footer>
 </div>
