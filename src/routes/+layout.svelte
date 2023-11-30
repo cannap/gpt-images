5e4e7ed558a2ae8da0a4a70a047fc8ba0c7944b4
Marko Bolliger <sh4fty@gmail.com>
2023-11-30 22:14:16 +0100

    done

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,8 +10,10 @@
 	};
 </script>
 
-<a c class:text-red-600={isActive('/')} href="{base}/">Index</a>
-<a class:text-red-600={isActive('/test')} href="{base}/test">Test page</a>
+<nav class="flex gap-3 fixed w-full top-0 bg-red-400">
+	<a class:text-red-600={isActive('/')} href="{base}/">Index</a>
+	<a class:text-red-600={isActive('/test')} href="{base}/test">Test page</a>
+</nav>
 <div class="flex flex-col h-screen">
 	<header class="bg-red-500">header</header>
 	<main class="bg-green-500 flex-grow"><slot /></main>
